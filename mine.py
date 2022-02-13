6455d10dd78f70f3655db28ba113b10ac43189ed
Clemensss <cass@ic.ufal.br>
2022-02-13 17:22:33 -0300

    new

--- mine.py
+++ mine.py
@@ -6,10 +6,15 @@ import curses
 level = [[8,8,10],[16,16,40],[16,30, 40]]
 class MineSweeper:
     global level
+
     #generate mined camp (without maked tiles)
-    BOMB = '*'
-    ROUND = 3
-    BLANK = 0
+    BOMB   = '*'
+    ROUND  = 3
+    NOBOMB = 0
+    BLANK  = ' ' 
+    FLAG   = '!' 
+    EMPTY  = '#'
+
     def __init__(self, dif, change=None):
 
         sizex, sizey, bombs = 0,0,0
@@ -21,23 +26,25 @@ class MineSweeper:
             sizex, sizey, bombs = change[1],change[0],change[2]
         
         self.bombs = bombs
-        self.ongoing = [['#' for _ in range(sizex)] for _ in range(sizey)]
+        self.ongoing = [[self.EMPTY for _ in range(sizex)] for _ in range(sizey)]
     
         self.mineGen(sizex, sizey, bombs)
 
     def mineGen(self, sizex, sizey, bombs):
-        board = [[0 for _ in range(sizex)] for _ in range(sizey)]
+        board = [[self.NOBOMB for _ in range(sizex)] for _ in range(sizey)]
+        self.score  = board
         l = []
         while(bombs):
             bombx = random.randint(0,sizex-1)
             bomby = random.randint(0,sizey-1)
 
-            if board[bomby][bombx] == 0:
+            if board[bomby][bombx] == self.NOBOMB:
                 board[bomby][bombx] = self.BOMB
                 l.append((bomby, bombx))
                 bombs-=1
 
         self.static = board
+        self.comp = board
         self.mineNum()
 
     #changes tiles around a bomb
@@ -63,9 +70,29 @@ class MineSweeper:
                 if self.static[i][j] == self.BOMB:
                      self.mineCheck(i, j)
     
+    def compareScore(self):
+        for i in range(len(self.score)):
+            for j in range(len(self.score[0])):
+                if self.score[i][j] != self.comp[i][j]:
+                    return 1
+        return 0 
+
+    def flag(self, y, x):
+        self.ongoing[y][x] = self.FLAG
+        self.score[y][x] = self.BOMB
+
+    def deflag(self, y, x):
+        self.ongoing[y][x] = self.EMPTY
+        self.score[y][x] = self.NOBOMB
+
     def reveal(self, y, x):
-        if self.static[y][x] == -1:
+        if self.static[y][x] == self.BOMB:
             return -1
+
+        elif self.static[y][x] > 0:
+            self.ongoing[y][x] = self.static[y][x]
+            return 0
+
         self.revealIter(y, x, [])
         return 0
     
@@ -79,12 +106,12 @@ class MineSweeper:
             for i in range(3):
                 for j in range(3):
                     if y1+i in max_y and x1+j in max_x and [y1+i, x1+j] not in prev:
-                        if self.static[y1+i][x1+j] == 0:
-                            self.ongoing[y1+i][x1+j] = ' '
+                        if self.static[y1+i][x1+j] == self.NOBOMB:
+                            self.ongoing[y1+i][x1+j] = self.BLANK
                             prev.append([y1+i, x1+j])
                             self.revealIter(y1+i, x1+j, prev)
 
-                        elif self.static[y1+i][x1+j] == -1:
+                        elif self.static[y1+i][x1+j] == self.BOMB:
                             prev.append([y1+i, x1+j])
 
                         else:
@@ -110,7 +137,6 @@ class Game:
 
         self.cursor = [0,0]
         self.stdscr = screen
-        self.index  = 0
         self.pair = 0
         self.max_y, self.max_x = self.stdscr.getmaxyx()
         self.mine = None
@@ -142,19 +168,51 @@ class Game:
 
         for i in range(len(arr)):
             p = 0
-            if self.index == i:
+            y, x = self.max_y//2+i, self.max_x//2 - len(arr[i])//2
+            if self.cursor[0] == i:
+
                 p = self.pair
-            self.stdscr.addstr(self.max_y//2+i, self.max_x//2 - len(arr[i])//2, arr[i], curses.color_pair(p))
+
+            self.stdscr.addstr(y, x, arr[i], curses.color_pair(p))
         self.stdscr.refresh()
 
     def makeMove(self, y, x):
         self.gamestatus = self.mine.reveal(y, x)
+    
+    def gameOver(self):
+
+        self.stdscr.clear()
+
+        lost = "LOOKS LIKE YOU LOST YOU PIECE OF SHIT"
+        press = "PRESS ANY KEY TO GO BACK TO THE MENU"
+
+        y, x = self.max_y//6, self.max_x//2
+        self.stdscr.addstr(y, x, lost, curses.color_pair(2))
+        self.stdscr.addstr(y+1, x, lost, curses.color_pair(2))
+
+        c = self.stdscr.getch()
+    def gameWon(self):
+
+        self.stdscr.clear()
+        lost = "LOOKS LIKE YOU WON YOU PIECE OF SHIT"
+        press = "PRESS ANY KEY TO GO BACK TO THE MENU"
+
+        y, x = self.max_y//2, self.max_x//2
+        self.stdscr.addstr(y, x, lost, curses.color_pair(2))
+        self.stdscr.addstr(y+1, x, lost, curses.color_pair(2))
+
+        c = self.stdscr.getch()
 
     def mineSweeper(self, dif, change=None):
 
         self.stdscr.clear()
         self.mine = MineSweeper(dif, change)
+
+        self.cursor = [0,0]
+
         while self.gamestatus == 0:
+
+            self.gamestatus = self.mine.compareScore()
 
             y, x = 0, self.max_x//4
             self.stdscr.addstr(y, x, "YOU ARE PLAYING MINESWEEPER", curses.color_pair(3))
@@ -176,16 +234,19 @@ class Game:
                 y, x = self.cursor[0], self.cursor[1]
                 self.makeMove(y, x)
                 
-            elif c == ord('m'):
-                pass
+            elif c == ord('f'):
+                self.mine.flag(self.cursor[0], self.cursor[1])
             elif c == ord('q'):
                 return 0
+
+
         #gameover  
         if self.gamestatus == -1:
-            pass
+            self.gameOver()
         #win
         elif self.gamestatus == 1:
-            pass
+            self.gameWon() 
+
     def menuNewGame(self):
 
 
@@ -194,7 +255,7 @@ class Game:
         y,x = self.max_y//2-3, self.max_x//2 - len(difficulty)//2
 
         y = 0
-        self.index = 0 
+        self.cursor = [0,0]
 
         self.stdscr.addstr(y, x, difficulty, curses.color_pair(2))
          
@@ -206,12 +267,14 @@ class Game:
             c = self.stdscr.getch()
 
             if c == curses.KEY_UP:
-                self.index= self.position(self.index,len(self.menu_nw), -1)
+                self.cursor[0] = self.position(self.cursor[0], len(self.menu_nw), -1)
             elif c == curses.KEY_DOWN:
-                self.index= self.position(self.index,len(self.menu_nw), 1)
+                self.cursor[0] = self.position(self.cursor[0], len(self.menu_nw), 1)
+
             elif c == ord('s'):
-                if self.index in  range(3):
-                    self.mineSweeper(self.index)
+                if self.cursor[0] in  range(3):
+                    self.mineSweeper(self.cursor[0])
+                    break
 
                 else:
                     pass
@@ -220,8 +283,8 @@ class Game:
                 return 0
 
     def menuStart(self):
-
-        self.index = 0 
+    
+        self.cursor = [0,0]
         
 
         while True:
@@ -234,16 +297,16 @@ class Game:
             c = self.stdscr.getch()
 
             if c == curses.KEY_UP:
-                self.index= self.position(self.index,len(self.menu_nw), -1)
+                self.cursor[0] = self.position(self.cursor[0], len(self.menu_strs), -1)
             elif c == curses.KEY_DOWN:
-                self.index= self.position(self.index,len(self.menu_nw), 1)
+                self.cursor[0] = self.position(self.cursor[0], len(self.menu_strs), 1)
 
             elif c == ord('s'):
-                if self.menu_strs[self.index] == "new game":
+                if self.menu_strs[self.cursor[0]] == "new game":
                     self.menuNewGame()
-                elif self.menu_strs[self.index] == "resume":
+                elif self.menu_strs[self.cursor[0]] == "resume":
                     pass
-                elif self.menu_strs[self.index] == "exit":
+                elif self.menu_strs[self.cursor[0]] == "exit":
                     break
 
             elif c == ord('q'):
